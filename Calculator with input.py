f7314dc622b2e481c4d4adb6ffb1158f2734e441
edgarisnew <167072642+edgarisnew@users.noreply.github.com>
2024-04-17 15:01:26 +0100

    Update Calculator with input.py
    Updated

--- Calculator with input.py
+++ Calculator with input.py
@@ -9,7 +9,7 @@ Welcome!"""
 
 print(message)
 print(x)
-print("Please remember: taxes have to be typed as 0.x and not a full number or a percentage.")
+print("Please remember: taxes have to be typed as a full number without the percentage symbol.")
 print(x)
 #insert your yearly income here
 yearly_income = int(input("What is your yearly income?"))
@@ -20,7 +20,7 @@ print(x)
 #taxes must be presented in the following way: 0.x 
 taxes = input("Whats the tax rate in your country?")
 
-taxes_b = 1-float(taxes)
+taxes_b = int(taxes) / 100
 
 print(taxes)
 print(x)
@@ -33,8 +33,8 @@ print(x)
 
 yearly_after = yearly_income * taxes_b
 
-if float(taxes)<0 or float(taxes)>1:
-   print("ERROR: Taxes cannot be under or above 1")
+if float(taxes)<0 or float(taxes)>100:
+   print("ERROR: Taxes cannot be under or above 100")
 
 else:
   print(yearly_after)
